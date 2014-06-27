4519f2c4ef2af10677785ed4d5aa4781f29cfa81
Faerbit <faerbit@gmail.com>
2014-06-28 01:06:35 +0200

    Now adding leading zeroes.

--- new_problem.sh
+++ new_problem.sh
@@ -2,8 +2,9 @@ if [[ $1 == "" ]]
 then
     echo "Give problem number"
 else
-    mkdir "problem_$1"
-    cd "problem_$1"
+    number=$(printf "%03d" $1)
+    mkdir "problem_$number"
+    cd "problem_$number"
     touch script.py
     echo -e "all: test\ntest:\n	python script.py" > Makefile
 fi
